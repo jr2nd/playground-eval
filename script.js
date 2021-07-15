cac8bafcbadf036de7e485f0bca93402251e4231
jr2nd <junior2nd@gmail.com>
2021-07-15 09:47:45 -0500

    2 files modified

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-const answer = document.querySelector('#evaluated')
+const answer = document.querySelector('#answer');
+const numbers = document.querySelectorAll('.key_number');
+const clear = document.querySelector('#equals')
+for (let i = 0; i < numbers.length; i++)
+  numbers[i].addEventListener('click', function() {
+    answer.innerHTML += this.innerHTML
+  });
 
