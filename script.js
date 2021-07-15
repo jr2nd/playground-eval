e9a17cd6cf324ac149a803164f1de18b547b9cd4
jr2nd <junior2nd@gmail.com>
2021-07-15 11:44:54 -0500

    2 files modified

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 const answer = document.querySelector('#answer');
 const numbers = document.querySelectorAll('.key_number');
 const equals = document.querySelector('#key_equals');
-const decimal = document.querySelector('#key_decimal')
+const decimal = document.querySelector('#key_decimal');
 const backspace = document.querySelector('#key_backspace');
+const clear = document.querySelector('#key_clear');
 
 for (let i = 0; i < numbers.length; i++)
   numbers[i].addEventListener('click', function() {
     answer.innerHTML += this.innerHTML;
-  });//numbers
+  }); //numbers
+
 equals.addEventListener('click', function() {
-  answer.innerHTML = eval(answer.innerHTML)
-});//equals
+  answer.innerHTML = eval(answer.innerHTML);
+}); //equals
 decimal.addEventListener('click', function() {
-if(parseFloat(answer.innerHTML) === parseInt(answer.innerHTML)){ 
-answer.innerHTML += this.innerHTML;
-});//decimal
-backspace.addEventListener('click', function() {});//backspace
+for(let i = 0; i < answer.innerHTML.length; i++){
+  if(answer.innerHTML.charCodeAt(i) === 46)return
+}//check for a decimal point already inthe string
+if(answer.innerHTML === ''){
+  answer.innerHTML = 0;
+}//if entry isless than 1 pad with a zero before the decimal
+answer.innerHTML += '.'
+}); //decimal
+backspace.addEventListener('click', function() {
+  let newAnswer = answer.innerHTML;
+  answer.innerHTML = newAnswer.slice(0, newAnswer.length - 1);
+}); //backspace
+clear.addEventListener('click', function() {
+  answer.innerHTML = '';
+});
