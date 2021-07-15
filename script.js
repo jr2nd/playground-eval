15ca711aaee5ae23d8e22d89fd02e3bbadc25f72
jr2nd <junior2nd@gmail.com>
2021-07-15 09:50:42 -0500

    1 file modified

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const answer = document.querySelector('#answer');
 const numbers = document.querySelectorAll('.key_number');
-const clear = document.querySelector('#equals')
+const clear = document.querySelector('#key_clear');
+const equals = document.querySelectorAll('#key_equals');
+const backspace = document.querySelectorAll('#key_backspace');
+
 for (let i = 0; i < numbers.length; i++)
   numbers[i].addEventListener('click', function() {
-    answer.innerHTML += this.innerHTML
+    answer.innerHTML += this.innerHTML;
   });
-
+clear.addEventListener('click', function() {});
+equals.addEventListener('click', function() {});
+backspace.addEventListener('click', function() {});
