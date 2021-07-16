b8558e2d7dab428379d7b2f424d325ea54dde025
jr2nd <junior2nd@gmail.com>
2021-07-15 22:49:20 -0500

    3 files modified

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const operators = document.querySelectorAll('.operator');
 for (let i = 0; i < numbers.length; i++)
   numbers[i].addEventListener('click', function() {
     answer.innerHTML += this.innerHTML;
-document.querySelector('#numin').value=this.innerHTML;
+document.querySelector('#number1').value=this.innerHTML;
   }); //numbers
 
 equals.addEventListener('click', function() {
